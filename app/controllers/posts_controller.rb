f1185afafa3935bb38caec9f8447bae2595460cd
w-yousuke <everyday10062000@yahoo.co.jp>
2021-09-18 08:41:03 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,7 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.order(id: "DESC")
   end
-
-  # def new
-  # end
 
   def create
     post = Post.create(content: params[:content])
